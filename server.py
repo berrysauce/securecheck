16ae255f1dfcb2311ad6483c7a49a89ccd33d11a
Paul <berrysauce.dev@gmail.com>
2021-03-05 21:41:24 +0100

    Add files via upload

--- server.py
+++ server.py
@@ -1,13 +1,15 @@
 import socket
-import datetime
+from datetime import datetime
+import os
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 # host = "localhost"
-host_ip = "149.28.63.177"
+host_name = socket.gethostname()
+host_ip = socket.gethostbyname(host_name)
 host_port = 15420
-print("IP :  ",host_ip)
-print("Port : ",host_port)
+print("Hostname :  ",host_name)
+print("IP : ",host_ip)
 
 sock.bind((host_ip,host_port))
 print("[...] Waiting for transmission")
@@ -23,7 +25,15 @@ while True:
 
     print(data)
     print("--------------------  [END OF TRANSMISSION]  --------------------")
-    with open("{}.txt".format(datetime.datetime.now()), "w") as myfile:
-        myfile.write(data)
+    try:
+        with open(addr[0]+"/"+addr[0], "r") as f:
+            f.read()
+    except IOError:
+        os.mkdir(addr[0])
+        with open(addr[0]+"/"+addr[0], "w") as f:
+            f.write(addr)
+    finally:
+        with open(addr[0]+"/"+"{0}.txt".format(datetime.now().strftime("%m/%d/%Y-%H:%M:%S")), "w") as f:
+            f.write(data)
     conn.close()
     print("[...] Closed connection from:", addr)
